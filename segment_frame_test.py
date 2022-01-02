b16694eff6bb3c2caa8a25e46ddd7b1170f0f73b
erosand <96470959+erosand@users.noreply.github.com>
2022-01-02 20:45:09 +0100

    Add files via upload
    Kalmanfilter fixat, ej tunat

--- segment_frame_test.py
+++ segment_frame_test.py
@@ -2,67 +2,16 @@ import cv2
 import argparse
 import os
 import numpy as np
+from numpy.core.fromnumeric import size
 import filterpy.kalman as kf 
 import filterpy.common as co
-from copy import copy
+from copy import Error, copy
 from math import pi
 
 from utils import Rectangle, Grab_Cut, Kmeans, Contour_Detection
 
-def get_measurement(mask, rectangle, prev_measurements):
 
-	mask_info = get_mask_info(mask, rectangle)
-
-	# räkna ut föränding i mätningar
-	# prev bör alltså inte innehålla förra hastigheterna
-	if prev_measurements is not None:
-		delta_measurements = [cur - prev for cur, prev in zip(mask_info, prev_measurements)]
-		delta_measurements = np.asarray(delta_measurements, dtype=np.float32)
-	else:
-		delta_measurements = None
-
-	mask_info = np.asarray(mask_info, dtype=np.float32)
-	
-
-	return mask_info, delta_measurements # borde kanske slå ihop?
-	
-
-def get_indices(mask):
-	index_list = []
-
-	X = np.shape(outputMask)[1]
-	Y = np.shape(outputMask)[0]
-	index_tot = [0,0]
-	for x in range(X):
-		for y in range(Y):
-			if outputMask[y, x] > 0:
-				index_list.append((x, y))
-
-				index_tot[0] += x
-				index_tot[1] += y
-
-	index_amount = len(index_list)
-
-	index_x, index_y = index_tot[0]//index_amount, index_tot[1]//index_amount
-	i_min, i_max = min(index_list), max(index_list)
-
-	return index_list, index_x, index_y, i_min, i_max, index_amount
-
-def get_mask_info(mask, rectangle):
-
-	index_list, index_x, index_y, i_min, i_max, index_amount = get_indices(mask)
-
-	mask_height = i_max[1] - i_min[1]
-	mask_width = i_max[0] - i_min[0]
-	mask_density = index_amount *4/(pi*mask_height*mask_width) # formel för ellipse, kanske bör ändras för att hantera outliers i pixlar?
-
-	index_x, index_y = index_x+rectangle[0], index_y+rectangle[1] # nu jobbar man inte med croppade koordinater längre
-	
-	return index_x, index_y, index_amount, mask_height, mask_width, mask_density
-
-if __name__=="__main__":
-	
-
+def main():
 	# arg parser, nyttja text 'python3 segment_frame_test.py -s Kmeans' för att köra med Kmeans metoden
 	ap = argparse.ArgumentParser()
 	ap.add_argument("-i", "--input", type=str,
@@ -93,24 +42,26 @@ if __name__=="__main__":
 	# ta första bilden ur videon
 	vidcap = cv2.VideoCapture(args["input"])
 	success,base_image = vidcap.read()
-	image = base_image
+	image = copy(base_image)
 
 	# Initiera Kalmanfilter
-	k_fil = kf.KalmanFilter(dim_x = 2, dim_z = 2, dim_u = 0) # Vill vi ha med hastighet som state också? isf ska dim_x = 4
-	# x0, y0 = getMeanPosition(mask) 
-	#k_fil.x = np.array([[x0],[y0],[vx0],[vy0]])	# Startposition och hastighet (x0,y0,vx0,vy0)
-	k_fil.F = np.array([[1.,1.],[0.,1.]])   # state transition matrix
-	prev_measurements = None
-
 	dt = 0.1
-
-	k_fil.H = np.array([[1.,0.]])           # Measurement function
-	k_fil.P *= 1000.                        # covariance matrix
-	k_fil.R = 5                             # state uncertainty
-	k_fil.Q = co.Q_discrete_white_noise(2, dt, .1) # process uncertainty
+	k_fil = kf.KalmanFilter(dim_x = 4, dim_z = 2, dim_u = 2) 
+	k_fil.F = np.array([[1.,0.,dt,0.], 		# state transition matrix
+						[0.,1.,0.,dt],
+						[0.,0.,0.,0.],
+						[0.,0.,0.,0.]])   
+	k_fil.H = np.array([[1.,0.,0.,0.],
+						[0.,1.,0.,0.]])    	# Measurement function
+	k_fil.P *= 1000.                        # Covariance matrix
+	k_fil.R = 5                             # State uncertainty
+	k_fil.Q = co.Q_discrete_white_noise(4, dt, .1) # Process uncertainty
+	prev_measurements = None
+	#print(k_fil)
 
 	count = 0
 	while success:
+		cv2.imshow(windowName, image) # visa bilden med eller utan rektangel
 		# kolla om rektangeln börjat bli vald och rita isf ut den
 		if rect.is_active():
 			p0, p1 = rect.get_points()
@@ -123,12 +74,22 @@ if __name__=="__main__":
 				#outputMask, output = Grab_Cut(base_image, selected_area, args["iter"])
 				
 				outputMask, _, new_selected_area = Segment(base_image, selected_area)
+				try:
+					measurements, delta_measurements = get_measurement(outputMask, new_selected_area, prev_measurements) # döp till prev measurements på direkten?
+				except ZeroDivisionError:
+					print('No foreground found, try again')
+					continue
+				#print("Measurements\n", measurements, "\nx coor,    y coor,    nr of indices,    height    ,width,    density")
+				x0, y0 = measurements[0], measurements[1]
+				#print(delta_measurements)
+				if delta_measurements is not None: 
+					v_x, v_y = delta_measurements[0], delta_measurements[1]
+				else: v_x, v_y = 0, 0
 
-				measurements, delta_measurements = get_measurement(outputMask, new_selected_area, prev_measurements) # döp till prev measurements på direkten?
-				print("Measurements\n", measurements, "\nx coor,    y coor,    nr of indices,    height    ,width,    density")
+				k_fil.x = np.array([[x0],[y0],[v_x],[v_y]])
+				#print(k_fil.x)
 				
-
-		cv2.imshow(windowName, image) # visa bilden med eller utan rektangel
+		
 
 		# stoppa eller gå till nästa frame beroende på space eller esc
 		k = cv2.waitKey(1)
@@ -138,17 +99,84 @@ if __name__=="__main__":
 
 		if k%256 == 32:
 			success,base_image = vidcap.read()
-			image = base_image
+			image = copy(base_image)
 			print('Read a new frame: ', success)
+			
+			try:
+				measurements, delta_measurements = get_measurement(outputMask, new_selected_area, prev_measurements) # döp till prev measurements på direkten?
+			except:
+				print('No rectangle drawn')
+				continue
+			z = measurements[0:2]
 
 			k_fil.predict()
-			#z = getMeasurement(image, mask)
-			#k_fil.update(z)
+			k_fil.update(z)
 			X = k_fil.x # New state estimate
 			P = k_fil.P # Covariance matrix
-
-			#plotEstimate(image,X,P)
+			X = X.astype(int)
+			# print(X, type(X))
+			# print(type(X[0]))
+			center_est = (int(X[0]),int(X[1]))
+			# print(center_est)
+			image = cv2.circle(image,center_est,20,(255,0,0),2)
 
 			count += 1
 
 	cv2.destroyAllWindows()
+
+
+def get_measurement(mask, rectangle, prev_measurements):
+	
+	mask_info = get_mask_info(mask, rectangle)
+
+	# räkna ut föränding i mätningar
+	# prev bör alltså inte innehålla förra hastigheterna
+	if prev_measurements is not None:
+		delta_measurements = [cur - prev for cur, prev in zip(mask_info, prev_measurements)]
+		delta_measurements = np.asarray(delta_measurements, dtype=np.float32)
+	else:
+		delta_measurements = None
+
+	mask_info = np.asarray(mask_info, dtype=np.float32)
+
+	return mask_info, delta_measurements # borde kanske slå ihop?
+	
+
+def get_indices(mask):
+	index_list = []
+	X = np.shape(mask)[1]
+	Y = np.shape(mask)[0]
+	index_tot = [0,0]
+	for x in range(X):
+		for y in range(Y):
+			mask[y,x] > 0
+			if (mask[y,x] > 0).any(): # Oklart varför .any() behövs när mask[y,x] är 1x1
+				index_list.append((x,y))
+
+				index_tot[0] += x
+				index_tot[1] += y
+
+	index_amount = len(index_list)
+	#if index_amount == 0: raise Exception('No foreground found :(')
+	index_x, index_y = index_tot[0]//index_amount, index_tot[1]//index_amount
+	i_min, i_max = min(index_list), max(index_list)
+
+	return index_list, index_x, index_y, i_min, i_max, index_amount
+
+
+def get_mask_info(mask, rectangle):
+
+	index_list, index_x, index_y, i_min, i_max, index_amount = get_indices(mask)
+
+	mask_height = i_max[1] - i_min[1]
+	mask_width = i_max[0] - i_min[0]
+	mask_density = index_amount *4/(pi*mask_height*mask_width) # formel för ellipse, kanske bör ändras för att hantera outliers i pixlar?
+
+	index_x, index_y = index_x+rectangle[0], index_y+rectangle[1] # nu jobbar man inte med croppade koordinater längre
+	
+	return index_x, index_y, index_amount, mask_height, mask_width, mask_density
+
+
+if __name__=="__main__":
+	main()
+
