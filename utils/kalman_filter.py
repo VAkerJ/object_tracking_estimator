8c4077b4f23b58dc907daf55ce3ca75af80f8d14
Erik A <erik@anderbergarna.com>
2022-01-04 00:54:17 +0100

    Rektangeluppdatering finns men behöver tunas

--- utils/kalman_filter.py
+++ utils/kalman_filter.py
@@ -6,13 +6,13 @@ class Filter():
 
 	def __init__(self, measurements, delta_measurements): 
 		dt = 0.1
-		process_var = 10
-		measurement_var = 10
+		process_var = 1
+		measurement_var = 0.1
 		k_fil = kf.KalmanFilter(dim_x = 4, dim_z = 2, dim_u = 2) 
 		k_fil.F = np.array([[1.,0.,dt,0.], 		# state transition matrix
 							[0.,1.,0.,dt],
-							[0.,0.,0.,0.],
-							[0.,0.,0.,0.]])   
+							[0.,0.,1.,0.],
+							[0.,0.,0.,1.]])   
 		k_fil.H = np.array([[1.,0.,0.,0.],		# Measurement function
 							[0.,1.,0.,0.]])    	
 		k_fil.B = np.array([[0.,0.],			# Control transition matrix
@@ -20,12 +20,14 @@ class Filter():
 							[1.,0.],
 							[0.,1.]])
 		k_fil.P *= 1000.                   		# Covariance matrix
-		k_fil.Q = np.diag(np.random.standard_normal(4))*process_var 	# Process uncertainty/noise (variance)
-		k_fil.R = np.diag(np.random.standard_normal(2))*measurement_var	# Measurment uncertainty/noise (variance)
-
+		# k_fil.Q = np.diag(np.random.standard_normal(4))*process_var 	# Process uncertainty/noise (variance)
+		# k_fil.R = np.diag(np.random.standard_normal(2))*measurement_var	# Measurment uncertainty/noise (variance)
+		k_fil.Q = np.eye(4)*process_var 	# Process uncertainty/noise (variance)
+		k_fil.R = np.eye(2)*measurement_var	# Measurment uncertainty/noise (variance)
 		self.dt, self.k_fil, self.prev_measurements = dt, k_fil, None
+		self.delta_measurements = delta_measurements
 		self.set_x(measurements, delta_measurements)
-		print(k_fil)
+		# print(k_fil)
 
 	def set_x(self, measurements, delta_measurements):
 		x0, y0 = measurements[0], measurements[1]
@@ -39,10 +41,9 @@ class Filter():
 		self.k_fil.predict(u)
 		z = measurements[0:2]
 		self.k_fil.update(z)
-		print(self.k_fil.x,'\n')
-		print(self.k_fil.K)
-
-		
+		# print(self.k_fil.x,'\n')
+		# print(self.k_fil.K)
+		self.delta_measurements = delta_measurements
 
 	def set_selected_area(self, selected_area):
 		P = []
@@ -51,8 +52,21 @@ class Filter():
 			X.append(int(self.k_fil.x[i]))
 			P.append(float(self.k_fil.P.diagonal()[i]))
 		center_est = X[0:2]
-
 		(x_min, y_min, x_len, y_len) = selected_area
+
+		delta_density = self.delta_measurements[5]
+		print(delta_density)
+		dxy = 1
+		lim = 0
+		if delta_density > lim:
+			x_len += dxy
+			y_len += dxy
+		elif delta_density < lim:
+			x_len -= dxy
+			y_len -= dxy
+		
+		print(x_len,y_len)
+
 		x_min = int(center_est[0] - x_len/2)
 		y_min = int(center_est[1] - y_len/2)
 		selected_area = (x_min, y_min, x_len, y_len)
